0deb799dfa068ff6017d9ee5ad85e97d44d4d0b8
Misha Causur <mishacausur@mail.ru>
2022-06-17 23:33:55 +0500

    Initial Commit

---
+++ stoner/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  stoner
+//
+//  Created by Misha Causur on 17.06.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
