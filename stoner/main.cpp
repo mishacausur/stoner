3b87ce5d2283b3150351a49eeb1f94d9ee7ddee1
Misha Causur <mishacausur@mail.ru>
2022-06-17 23:38:39 +0500

    task 1 is done

--- stoner/main.cpp
+++ stoner/main.cpp
@@ -6,9 +6,14 @@
 //
 
 #include <iostream>
+using namespace std;
+
+namespace aster {
+    int i = 1;
+    float f = 3.1413;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    cout << aster::i << " " << aster::f << endl;
     return 0;
 }
