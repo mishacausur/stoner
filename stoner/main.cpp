4637d9db4837bbe26740ca52da9f16a963b1487b
Misha Causur <mishacausur@mail.ru>
2022-06-17 23:45:08 +0500

    task 2 - struct implemented

--- stoner/main.cpp
+++ stoner/main.cpp
@@ -8,12 +8,24 @@
 #include <iostream>
 using namespace std;
 
-namespace aster {
-    int i = 1;
-    float f = 3.1413;
-}
+struct TPoint {
+    int x;
+private:
+    int y;
+public:
+    void SetY(int y1) {
+        y = y1;
+    }
+    int GetY() {
+        return y;
+    }
+};
 
-int main(int argc, const char * argv[]) {
-    cout << aster::i << " " << aster::f << endl;
+int main() {
+    TPoint p;
+    p.x = 10;
+    p.SetY(44);
+    cout << p.x << " " << p.GetY() << endl;
+    
     return 0;
 }
