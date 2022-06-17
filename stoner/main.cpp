1e089e4b05bff73c4354a0eb71f254e280064598
Misha Causur <mishacausur@mail.ru>
2022-06-17 23:55:34 +0500

    hwT2 is done

--- stoner/main.cpp
+++ stoner/main.cpp
@@ -24,11 +24,41 @@ int TPoint::GetY() {
     return y;
 }
 
+ // HW
+
+class Setter {
+public:
+    int w;
+    void SetW(int w1);
+    int GetW();
+    int GetSQR();
+    int GetRT();
+};
+
+void Setter::SetW(int w1) {
+    w = w1;
+}
+
+void exer() {
+    Setter s;
+    s.SetW(4);
+   
+    cout << s.w << " " << s.GetW() << " " << s.GetSQR() << endl;
+}
+
+int Setter::GetW() {
+    return w;
+}
+
+int Setter::GetSQR() {
+    return w * w;
+}
+
 int main() {
     TPoint p;
     p.x = 10;
     p.SetY(44);
     cout << p.x << " " << p.GetY() << endl;
-    
+    exer();
     return 0;
 }
