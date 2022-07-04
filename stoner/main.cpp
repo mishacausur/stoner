c0cced5b4ca219392cb8d00c13aab5b4e9cd8233
Misha Causur <mishacausur@mail.ru>
2022-07-05 00:44:46 +0500

    malloc int added

--- stoner/main.cpp
+++ stoner/main.cpp
@@ -6,35 +6,14 @@
 //
 
 #include <iostream>
+
 using namespace std;
 
-// HW
-
-class TPoints {
-    int x;
-    int y;
-public:
-    TPoints(int _x, int _y) {
-        x = _x;
-        y = _y;
-    }
-    
-    int GetM();
-    
-    ~TPoints() {
-        cout << GetM() << endl;
-    }
-    
-};
-
-int TPoints::GetM() {
-    return  x - y;
-}
-
 int main() {
-    TPoints x(6, 3);
-    {
-        TPoints y(21, 5);
-    }
+    int *a = NULL;
+    a = (int*)malloc( sizeof(a));
+    *a = 5;
+    cout << *a << endl;
+    free(a);
     return 0;
 }
